affe78663691018dbb64229f5a43065ea0d2a313
sds7500 <somyasheti@gmail.com>
2021-11-27 09:50:34 +0530

    preorder postorder and height of tree

--- trees.java
+++ trees.java
@@ -9,21 +9,46 @@ class Node{
 }
 
 public class trees {
-
-    public static void inorder(Node root){
+    static void inorder(Node root){
         if(root!=null){
             inorder(root.left);
             System.out.print(root.data+" ");
             inorder(root.right);
         }
     }
+    static void preorder(Node root){
+        if(root!=null){
+            System.out.print(root.data+" ");
+            preorder(root.left);
+            preorder(root.right);
+        }
+    }
+    static void postorder(Node root){
+        if(root!=null){
+            postorder(root.left);
+            postorder(root.right);
+            System.out.print(root.data+" ");
+        }
+    }
+    static int height(Node root){
+        if(root==null)return 0;
+        return 1+Math.max(height(root.left),height(root.right));
+    } 
     public static void main(String args[]) 
     { 
         Node root=new Node(10);
     	root.left=new Node(20);
     	root.right=new Node(30);
     	root.left.left=new Node(40); 
-        System.out.print("Inorder traversal of tree is : ");
+        System.out.print("Inorder traversal of tree is: ");
         inorder(root);
+        System.out.println();
+        System.out.print("Preorder traversal of tree is: ");
+        preorder(root);
+        System.out.println();
+        System.out.print("Postorder traversal of tree is: ");
+        postorder(root);
+        System.out.println();
+        System.out.print("Height of binary tree is: "+height(root));
     } 
 }
